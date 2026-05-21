a73b159a7b59b16568c096981fe134e3a9d4342e
luffy <52489884+hashiq@users.noreply.github.com>
2026-05-21 10:24:42 +0400

    Refactor bidding process and add highest bid function
    Refactored bidding logic and added a function to determine the highest bid.

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
-game_continue = True
-value_star =0
-age = []
-bidders_list = []
-print("Welcom to the grand bid")
-while game_continue:
-    bider_name = input("Enter a bider name :")
-    bid_price = int(input("Enter a bid price :$"))
-    bidders_list.append({ "name" : bider_name, "age":bid_price})
-    bid_stop = input("Do you want to continue the bid 'yes' or 'no'").lower()
-    if bid_stop == 'yes':
+loop_run = True
+bid = {}
+
+def highest_bider(bid):
+    max_bid = 0
+    for i in bid:
+       if max_bid < bid[i]:
+           max_bid=bid[i]
+    return max_bid
+
+
+while loop_run:
+    bider = input("enter your name :")
+    price = int(input("enter bid price :"))
+    bid[bider]=price
+    stop_bid = input("does any other bider need to join :")
+    if stop_bid== "yes":
         pass
-    elif bid_stop == 'no':
-        game_continue = False
-        for i in range(len(bidders_list)):
-            age.append(bidders_list[i]["age"])
-        max_age = max(age)
-        min_age = min(age)
-        for i in range(len(bidders_list)):
-            user_founded = bidders_list[i]
-            if max_age == bidders_list[i]["age"]:
-                print(f"'{user_founded['name']}'age'{user_founded['age']}'")
-
-            elif min_age == bidders_list[i]["age"]:
-                print(f" Lowest bider :{user_founded['name']} -'{user_founded['age']}")
-    else:
-        game_continue = False
-        print("#404 error ")
-
-
-
+    elif stop_bid =="no":
+        print(highest_bider(bid))
+        loop_run=False
+    else:print("Wrong input")
